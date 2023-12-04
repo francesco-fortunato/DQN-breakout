3b84a31ab62471e904b3b176efe5a53286350777
francesco-fortunato <francesco.fortunato1999@gmail.com>
2023-12-04 09:58:42 +0100

    change from pytorch to tensorflow

--- breakout.py
+++ breakout.py
@@ -1,166 +1,106 @@
+import os
+
 import gym
 import numpy as np
 import random
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from collections import deque
-import cv2
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import matplotlib.pyplot as plt
-import csv
-import logging
-import time
 
-# Set seed for reproducibility
-seed = 42
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.backends.cudnn.deterministic = True
+# Define the Breakout environment
+env = gym.make("ALE/Breakout-v5", render_mode="human")
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Constants
+STATE_SHAPE = (210, 160, 3)
+NUM_ACTIONS = 18
+MEMORY_SIZE = 10000
+BATCH_SIZE = 32
+GAMMA = 0.99  # Discount factor
+EPSILON_START = 1.0
+EPSILON_MIN = 0.01
+EPSILON_DECAY = 0.995
+TARGET_UPDATE_FREQ = 1000  # Update target network every X steps
 
-# Define a simple feedforward neural network
-class QNetwork(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(QNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_size, 32)
-        self.fc2 = nn.Linear(32, 16)
-        self.fc3 = nn.Linear(16, output_size)
+# Initialize replay memory
+replay_memory = deque(maxlen=MEMORY_SIZE)
 
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        return self.fc3(x)
+# Define Q-Network
+model = Sequential()
+model.add(Dense(32, input_shape=STATE_SHAPE, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(NUM_ACTIONS, activation='linear'))
+model.compile(optimizer='adam', loss='mse')
 
-# Initialize Breakout-v5 environment
-env = gym.make("ALE/Breakout-v5", render_mode="human", frameskip=(2,3))
+# Initialize target Q-Network with the same architecture
+target_model = Sequential.from_config(model.get_config())
+target_model.set_weights(model.get_weights())
 
-# Define hyperparameters
-epsilon = 1.0  # Exploration rate
-epsilon_min = 0.1
-epsilon_decay = 0.995
-gamma = 0.99  # Discount factor
-batch_size = 32
-memory = deque(maxlen=1000000)
-num_episodes = 1000
-target_update_frequency = 10
-learning_rate = 0.00025
+# Initialize exploration rate
+epsilon = EPSILON_START
 
-# Initialize the Q-networks (target and online)
-input_size = 210 * 160
-output_size = 4  # Action space size
-online_net = QNetwork(input_size, output_size)
-target_net = QNetwork(input_size, output_size)
-target_net.load_state_dict(online_net.state_dict())
-target_net.eval()
-
-# Define loss function and optimizer
-criterion = nn.MSELoss()
-optimizer = optim.Adam(online_net.parameters(), lr=learning_rate)
-
-# Initialize lists for logging
-episode_rewards = []
-avg_rewards = []
+# Training parameters
+total_episodes = 10000  # Total number of episodes to train
+max_steps_per_episode = 1000  # Maximum number of steps per episode
 
 # Training loop
-for episode in range(num_episodes):
-    start_time = time.time()  # Record the start time for the episode
-
+for episode in range(total_episodes):
     state = env.reset()
-    state = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)  # Convert to grayscale
-    state = torch.tensor(state, dtype=torch.float32).view(-1)
-
+    state = np.reshape(state, (1, *STATE_SHAPE))
     done = False
     total_reward = 0
 
-    episode_q_values = []  # List to store Q-values for this episode
-
-
-    while not done:
-        if random.random() < epsilon:
-            
+    for step in range(max_steps_per_episode):
+        # Exploration vs. exploitation
+        if np.random.rand() < epsilon:
             action = env.action_space.sample()
         else:
-            with torch.no_grad():
-                q_values = online_net(state)
-                action = torch.argmax(q_values).item()
-
-
+            Q_values = model.predict(state)
+            action = np.argmax(Q_values)
 
         next_state, reward, done, _ = env.step(action)
-        next_state = cv2.cvtColor(next_state, cv2.COLOR_RGB2GRAY)
-        next_state = torch.tensor(next_state, dtype=torch.float32).view(-1)
+        next_state = np.reshape(next_state, (1, *STATE_SHAPE))
 
-        # Store the experience in replay memory
-        memory.append((state, action, reward, next_state, done))
-
-        state = next_state
         total_reward += reward
 
-        if len(memory) >= batch_size:
-            batch = random.sample(memory, batch_size)
-            states, actions, rewards, next_states, dones = zip(*batch)
+        # Store experience in replay memory
+        replay_memory.append((state, action, reward, next_state, done))
 
-            states = torch.stack(states)
-            next_states = torch.stack(next_states)
-            q_values = online_net(states)
-            next_q_values = target_net(next_states)
+        # Update state
+        state = next_state
 
-            targets = q_values.clone()
-            for i in range(batch_size):
+        # Sample a random minibatch from replay memory and perform gradient descent
+        if len(replay_memory) >= BATCH_SIZE:
+            minibatch = random.sample(replay_memory, BATCH_SIZE)
+
+            states, actions, rewards, next_states, dones = zip(*minibatch)
+
+            states = np.vstack(states)
+            next_states = np.vstack(next_states)
+
+            target_Qs = model.predict(states)
+            target_Qs_next = target_model.predict(next_states)
+
+            for i in range(BATCH_SIZE):
                 if dones[i]:
-                    targets[i][actions[i]] = rewards[i]
+                    target_Qs[i, actions[i]] = rewards[i]
                 else:
-                    targets[i][actions[i]] = rewards[i] + gamma * torch.max(next_q_values[i])
+                    target_Qs[i, actions[i]] = rewards[i] + GAMMA * np.max(target_Qs_next[i])
 
-            optimizer.zero_grad()
-            loss = criterion(q_values, targets)
-            loss.backward()
-            optimizer.step()
+            model.fit(states, target_Qs, epochs=1, verbose=0)
 
-    # Create or open a log.txt file for writing
-    with open("log.txt", "a") as log_file:
+        # Update target network if needed
+        if step % TARGET_UPDATE_FREQ == 0:
+            target_model.set_weights(model.get_weights())
 
-        episode_rewards.append(total_reward)
-        epsilon = max(epsilon * epsilon_decay, epsilon_min)
+        if done:
+            break
 
-        # Update target network
-        if episode % target_update_frequency == 0:
-            target_net.load_state_dict(online_net.state_dict())
+    # Decay exploration rate
+    epsilon = max(EPSILON_MIN, epsilon * EPSILON_DECAY)
 
-        # Log and print results
-        episode_rewards.append(total_reward)
-        epsilon = max(epsilon * epsilon_decay, epsilon_min)
-
-        # Update target network
-        if episode % target_update_frequency == 0:
-            target_net.load_state_dict(online_net.state_dict())
-
-        # Calculate episode duration and log results
-        episode_duration = time.time() - start_time
-        avg_reward = np.mean(episode_rewards[-10:])
-        avg_rewards.append(avg_reward)
-
-        avg_q_value = np.mean(episode_q_values)
-
-
-        # Create a string with episode-specific information
-        episode_info = (f"Episode {episode}/{num_episodes}, Total Reward: {total_reward}, Last 10 Avg Reward: {avg_reward}, "
-                        f"Duration: {episode_duration:.2f} seconds\n")
-
-        # Write the episode information to the log file
-        log_file.write(episode_info)
-
-# Plot the rewards
-plt.plot(episode_rewards, label="Episode Reward")
-plt.plot(avg_rewards, label="Avg Reward (Last 10 Episodes)")
-plt.xlabel("Episode")
-plt.ylabel("Reward")
-plt.legend()
-plt.show()
+    # Logging
+    print(f"Episode: {episode + 1}, Total Reward: {total_reward}, Epsilon: {epsilon}")
 
 # Save the trained model
-MODEL_FILE = "./dqn_model"
-torch.save(online_net.state_dict(), MODEL_FILE)
+model.save("breakout_dqn_model.h5")
